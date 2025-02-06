c0f30a0c7f696bf0670f2f337da9f46b900e2112
ryny23 <166703602+ryny23@users.noreply.github.com>
2025-02-06 04:31:11 +0100

    Add files via upload

---
+++ Livreur_connect.java
@@ -0,0 +1,60 @@
+package pizzaInnodbConnect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Livreur_connect {
+	
+	public static void main(String[] args) {
+		 	
+		System.out.println("connection a mysql pizzaboxinnodb");
+			
+		String url = "jdbc:mysql://localhost:3306/pizzaboxinnodb?allowPublicKeyRetrieval=true&useSSL=false";
+		
+		String login = "root";
+		String passwd = "";
+		
+		Connection cn =null; 
+		Statement st =null;
+		ResultSet rs =null;
+ 
+		try {
+			// Etape 1 : Chargement du driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			// Etape 2 : r�cup�ration de la connexion
+			cn = DriverManager.getConnection(url, login, passwd);
+
+			// Etape 3 : Cr�ation d'un statement
+			st = cn.createStatement();
+
+			String sql = "SELECT * FROM LIVREUR  "   ;
+
+			// Etape 4 : ex�cution requ�te
+			rs = st.executeQuery(sql);
+			
+			// Si r�cup donn�es alors �tapes 5 (parcours Resultset)
+			System.out.println(rs);
+			
+			while (rs.next()) {
+				System.out.println("-----------------------------------");
+				System.out.println(rs.getString("NOMLIVR"));
+				System.out.println(rs.getString("PRENOMLIVR"));
+				System.out.println(rs.getString("DATEEMBAUCHLIVR"));
+			}
+			 
+			
+		} catch (ClassNotFoundException e) {//si il y un probleme de driver
+		 
+			e.printStackTrace();
+		} catch (SQLException e) {//si il y un probleme de sql
+	 
+			e.printStackTrace();
+		}
+
+	}
+
+}
